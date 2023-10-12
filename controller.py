1131cfa85da9fc42fdef176e77637956221a15c2
TheNiska <denis.bond.x@gmail.com>
2023-10-12 17:19:39 +0300

    rewrite with sqlite3, added wrapper

--- controller.py
+++ controller.py
@@ -1,63 +1,63 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Session
+from sqlite3 import connect
 from hashlib import sha256
 from funcs import create_new_pair
 
-engine = create_engine("sqlite:///users.db")
-conn = engine.connect()
+
+def manage_connection(func):
+    def wrapper(*args, **kwargs):
+        con = connect("users.db")
+        cur = con.cursor()
+        result = None
+
+        try:
+            result = func(*args, **kwargs, cur=cur)
+            con.commit()
+        except Exception as ex:
+            print(type(ex).__name__, ': ', ex, sep='')
+        else:
+            print('Success!')
+        finally:
+            con.close()
+
+        return result
+    return wrapper
 
 
-class Base(DeclarativeBase):
-    pass
+@manage_connection
+def execute_query(query: str, cur=None):
+    return cur.execute(query)
 
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(70), index=True, unique=True)
-    password_hash = Column(String(64))
-    private_key = Column(String(2000))
-    public_key = Column(String(500))
-
-
-Base.metadata.create_all(bind=engine)
-
-
-def register_user(username: str, password: str) -> None:
+@manage_connection
+def register_user(username: str, password: str, cur=None) -> None:
     hashed_password = sha256(password.encode('utf-8')).hexdigest()
     private_key, public_key = create_new_pair(password)
-    print(len(private_key))
-    print(len(public_key))
 
-    user = User(
-        username=username,
-        password_hash=hashed_password,
-        private_key=private_key,
-        public_key=public_key
-    )
+    fields = "(username, password_hash, private_key, public_key)"
+    values = f"('{username}', '{hashed_password}', " \
+             f"'{private_key}', '{public_key}')"
 
-    with Session(autoflush=False, bind=engine) as db:
-        db.add(user)
-        db.commit()
+    query = f"insert into users {fields} values {values}"
+    cur.execute(query)
 
 
-def get_private_key(username: str) -> str:
+@manage_connection
+def get_private_key(username: str, cur=None) -> str:
     '''Returns encrypted private key by username in 'str' format'''
 
-    query = text(f"select private_key from users where username='{username}'")
-    return conn.execute(query).first().private_key
+    query = f"select private_key from users where username='{username}'"
+    return cur.execute(query).fetchone()[0]
 
 
-def get_public_key(username: str) -> str:
+@manage_connection
+def get_public_key(username: str, cur=None) -> str:
     '''Returns public key by username in 'str' format'''
 
-    query = text(f"select public_key from users where username='{username}'")
-    return conn.execute(query).first().public_key
+    query = f"select public_key from users where username='{username}'"
+    return cur.execute(query).fetchone()[0]
 
 
-# register_user('TheNiska', '2012')
-# register_user('Dude', '00')
-# register_user('July', '1234')
+if __name__ == '__main__':
+    query = ""
+    res = execute_query(query)
+    print(res)
